71050d3a2190b3a075aa529f103d12fa87b24e90
Javier Oviedo <javieroviedo@192.168.1.85>
2024-12-21 11:38:06 -0300

    favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-
+import Image from 'next/image';
 export default function Home() {
   const router = useRouter();
 
@@ -11,7 +11,14 @@ export default function Home() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#2D303E] text-white">
-      <p>Redirecting to login...</p>
+    <div className="animate-bounce">
+      <Image
+        src="/public/favicon.png" 
+        alt="Loading isotipo"
+        width={20} 
+        height={20} 
+      />
     </div>
+  </div>
   );
 }
